31c13bd16bbc4757739de75c77bc91e760b25270
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-01-24 19:36:36 +0530

    Add files via upload

---
+++ NODEJS/app.js
@@ -0,0 +1,36 @@
+//  //First code
+// console.log('Google is Fantastic')
+
+
+
+
+// //function calling
+// function sayhello(){
+//     console.log('Hello! its Srisanjana')
+// }
+// sayhello()
+
+
+
+// //global object
+// console.log(global)
+
+
+
+// //finding if global prints the variable
+// let name = 'sri'
+
+// console.log(global.name)
+
+
+// //Modules and Modularity
+// let age = 18
+// console.log(global.age)
+
+
+// // calculator module
+const calculator = require('./calculator')
+calculator.addition (3, 6)
+calculator.substraction (10 , 7)
+calculator.multiplication (5 , 8)
+calculator.division (100 , 10)
